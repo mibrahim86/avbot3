b6c114eb9f6d89480cf27b480cb7666cdf846967
mibrahim86 <124355798+mibrahim86@users.noreply.github.com>
2023-02-03 17:56:41 +0500

    Update general_test.py

--- test/general_test.py
+++ test/general_test.py
@@ -4,7 +4,23 @@ import discord.ext.test as dpytest
 
 @pytest.mark.asyncio
 async def test_ping(bot):
-    await bot.load_extension("cogs.general")    
-    dpytest.configure(bot)
-    await dpytest.message("av!ping")
-    assert dpytest.verify().message().contains().content("Pong!")
+    try:
+        await bot.load_extension("cogs.General")
+    except Exception as e:
+        print(f"Error loading extension: {e}")
+        return
+    try:
+        dpytest.configure(bot)
+    except Exception as e:
+        print(f"Error configuring dpytest: {e}")
+        return
+    try:
+        await dpytest.message("av!ping")
+    except Exception as e:
+        print(f"Error sending message: {e}")
+        return
+    try:
+        assert dpytest.verify().message().contains().content("Pong!")
+    except Exception as e:
+        print(f"Error verifying message: {e}")
+        return
